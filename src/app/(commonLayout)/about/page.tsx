7419e56677b7b3da797f7d9cdd77afabbf9c7da3
unknown <antuvhai@gmail.com>
2026-01-19 22:14:39 +0600

    updated route

--- src/app/(commonLayout)/about/page.tsx
+++ src/app/(commonLayout)/about/page.tsx
@@ -3,7 +3,9 @@ export const dynamic = "force-dynamic";
 const AboutPage = async () => {
   await new Promise((resolve) => setTimeout(resolve, 4000));
   // throw new Error("something went wrong..");
-  return <div className="text-3xl">hello about page......</div>;
+  return <div className="text-3xl">
+    <h1 className="text-3xl text-blue-500 font-bold text-center">Hello Prisma Blog This is about Route.....🙂</h1>
+  </div>;
 };
 
 export default AboutPage;
--- src/app/(commonLayout)/contact/page.tsx
+++ src/app/(commonLayout)/contact/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ContactPage = () => {
   return (
     <div>
-      helloooo contact page.......
+      <h1 className="text-3xl text-blue-500 font-bold text-center">Hello Prisma Blog This is contact Route.....🙂</h1>
     </div>
   )
 }
