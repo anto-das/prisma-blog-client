bd49c01ddecdb507c5ee94989f533acc0c9f0566
unknown <antuvhai@gmail.com>
2026-01-22 23:24:46 +0600

    cookie

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,8 +1,18 @@
 import { authClient } from "@/lib/auth-client";
+import { cookies } from "next/headers";
 
 export default async function Home() {
-  const session = await authClient.getSession();
-  console.log(session)
+  const cookieStore = await cookies();
+  console.log(cookieStore.toString());
+  // const session = await authClient.getSession();
+
+  const res = await fetch("http://localhost:5000/api/auth/get-session", {
+    headers: {
+      Cookie: cookieStore.toString(),
+    },
+    cache: "no-store",
+  });
+  console.log(await res.json());
   return (
     <div>
       <h1 className="text-3xl text-blue-500 font-bold text-center">
