5ac9036381aa018a16f2cac0df5ff14e3148fc7f
unknown <antuvhai@gmail.com>
2026-01-24 23:30:42 +0600

    updated

--- src/app/(commonLayout)/about/page.tsx
+++ src/app/(commonLayout)/about/page.tsx
@@ -4,7 +4,7 @@ const AboutPage = async () => {
   await new Promise((resolve) => setTimeout(resolve, 4000));
   // throw new Error("something went wrong..");
   return <div className="text-3xl">
-    <h1 className="text-3xl text-blue-500 font-bold text-center">Hello Prisma Blog This is about Route.....🙂</h1>
+    <h1 className="text-3xl text-blue-500 font-bold text-center">Hello Prisma Blog This is a about Route.....🙂</h1>
   </div>;
 };
 
