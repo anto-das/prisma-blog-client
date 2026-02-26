ff40b7f462d909828c9d96d08cc89b9e01780601
unknown <antuvhai@gmail.com>
2026-02-26 10:19:57 +0600

    explore login and sign concept with better auth

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -2,17 +2,22 @@ import { authClient } from "@/lib/auth-client";
 import { cookies } from "next/headers";
 
 export default async function Home() {
-  const cookieStore = await cookies();
-  // console.log(cookieStore.getAll());
-  // const session = await authClient.getSession();
+  const session = await authClient.getSession();
+  console.log(session)
+  // const cookieStore = await cookies();
+  // // console.log(cookieStore.getAll());
+  // // const session = await authClient.getSession();
 
-  const res = await fetch("http://localhost:5000/api/auth/get-session", {
-    headers: {
-      Cookie: cookieStore.toString(),
-    },
-    cache: "no-store",
-  });
-  console.log(await res.json());
+  // const res = await fetch(
+  //   "https://medi-store-eight.vercel.app/api/auth/get-session",
+  //   {
+  //     headers: {
+  //       Cookie: cookieStore.toString(),
+  //     },
+  //     cache: "no-store",
+  //   },
+  // );
+  // console.log(await res.json());
   return (
     <div>
       <h1 className="text-3xl text-blue-500 font-bold text-center">
