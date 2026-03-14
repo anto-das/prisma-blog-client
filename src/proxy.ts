3101c654062aec9d08b142feb7c3a81da8ce5d1c
unknown <antuvhai@gmail.com>
2026-03-14 08:40:37 +0600

    added proxy.ts protected route

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,19 +1,38 @@
 import { NextRequest, NextResponse } from "next/server";
 import { userServices } from "./services/user.service";
+import { Roles } from "./constants/Roles";
 
 export async function proxy(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
   let isAuthenticate = false;
   let isAdmin = false;
   const { data } = await userServices.getSession();
-
+  console.log(data);
   if (data) {
     isAuthenticate = true;
+    isAdmin = data.user.role === Roles.admin;
   }
-  if()
+  if (!isAuthenticate) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
 
+  if (isAdmin && pathname.startsWith("/dashboard")) {
+    return NextResponse.redirect(new URL("/admin-dashboard", request.url));
+  }
+
+  if (!isAdmin && pathname.startsWith("/admin-dashboard")) {
+    return NextResponse.redirect(new URL("/user-dashboard", request.url));
+  }
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/dashboard"],
+  matcher: [
+    "/dashboard",
+    "/dashboard/:path*",
+    "/admin-dashboard",
+    "/admin-dashboard/:path*",
+    "/user-dashboard",
+    "/user-dashboard/:path*",
+  ],
 };
